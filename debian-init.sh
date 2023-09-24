2ac5f1106750157a7db92614b7e7eb6375306c98
Isaac Turner <spacey_sooty@outlook.com>
2023-09-24 09:33:06 +0800

    add linking to debian init

--- debian-init.sh
+++ debian-init.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
+
+# install
 curl https://sh.rustup.rs -sSf | sh
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar -xf nvim-linux64.tar.gz
@@ -7,3 +9,9 @@ source ~/.bashrc
 nvm install --lts
 npm install -g pnpm
 npm install -g turbo
+
+# link
+ln -s ~/Dotfiles/nvim ~/.config/nvim
+ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Dotfiles/.bashrc ~/.bashrc
+
