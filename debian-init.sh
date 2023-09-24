6cadd6458a1dfbb4fcdda02d305a681d93b96e9c
Isaac Turner <spacey_sooty@outlook.com>
2023-09-24 19:54:54 +1000

    add ripgrep install

--- debian-init.sh
+++ debian-init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 
+# perms
+chmod +x `ls scripts/*.sh`
+
 # install
 curl https://sh.rustup.rs -sSf | sh
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -9,9 +12,12 @@ source ~/.bashrc
 nvm install --lts
 npm install -g pnpm
 npm install -g turbo
+sudo apt-get install ripgrep
 
 # link
 ln -s ~/Dotfiles/nvim ~/.config/nvim
 ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Dotfiles/.bashrc ~/.bashrc
+
+rm nvim-linux64.tar.gz
 
