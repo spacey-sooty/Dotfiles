251a172c73a8d570916d9cf3a758bd7be1aba8e5
Isaac Turner <spacey_sooty@outlook.com>
2023-09-30 22:12:28 +0800

    update netrw

--- nvim/after/plugin/netrw.lua
+++ nvim/after/plugin/netrw.lua
@@ -1,6 +1,10 @@
+-- remove stupid banner
 vim.g.netrw_banner=0
--- todo fix hide gitignored items in netrw
--- vim.g.netrw_list_hide=netrw_gitignor#Hide()
+
+-- ignore gitignored files and .git directory
+vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']() .. [[.git/]]
+
+-- bind n to create new file and r to rename
 vim.api.nvim_create_autocmd('filetype', {
   pattern = 'netrw',
   desc = 'Better mappings for netrw',
@@ -16,4 +20,7 @@ vim.api.nvim_create_autocmd('filetype', {
     bind('r', 'R')
   end
 })
+
+-- human readable sizes like 5K or 3G instead of bytes
+vim.g.netrw_sizestyle = 'h'
 
