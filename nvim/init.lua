d0577741802a251e8a2d945a5b5e10c1c9e026e1
Isaac Turner <spacey_sooty@outlook.com>
2023-10-02 15:44:20 +1000

    add editor config (#3)

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	"editorconfig/editorconfig-vim",
 	"catppuccin/nvim",
 	"christoomey/vim-tmux-navigator", "nvim-lua/plenary.nvim",
 	"nvim-telescope/telescope.nvim",
