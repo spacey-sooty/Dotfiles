996daa4155ebcd287b721d5e93531020449f4258
Isaac Turner <spacey_sooty@outlook.com>
2023-10-03 21:18:23 +0800

    svelte

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'clangd', 'tsserver', 'rust_analyzer'},
+  ensure_installed = { 'clangd', 'tsserver', 'rust_analyzer', 'svelte' },
   handlers = {
     lsp_zero.default_setup,
   }
