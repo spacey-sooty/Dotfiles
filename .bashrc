9128d7d864503bda6168204af48283d6542e0295
Isaac Turner <spacey_sooty@outlook.com>
2023-10-22 13:50:45 +0800

    fix bash parser (#12)

--- .bashrc
+++ .bashrc
@@ -129,3 +129,4 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 # pnpm end
+
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "markdown", "lua", "scss", "typescript", "rust" },
+	ensure_installed = { "c", "markdown", "lua", "bash", "scss", "typescript", "rust" },
 	sync_install = false,
 	highlight = { enable = true },
 	indent = { enable = true },
