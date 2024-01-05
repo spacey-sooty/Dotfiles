abd2ab4592ee0f937d64c6b9ea61ad28fd034b7f
Isaac Turner <spacey_sooty@outlook.com>
2024-01-05 17:20:34 +0800

    epic code actions

--- nvim/lua/spacey-sooty/lsp.lua
+++ nvim/lua/spacey-sooty/lsp.lua
@@ -106,3 +106,14 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+local opts = { noremap=true, silent=true }
+
+local function quickfix()
+    vim.lsp.buf.code_action({
+        filter = function(a) return a.isPreferred end,
+        apply = true
+    })
+end
+
+vim.keymap.set('n', '<leader>qf', quickfix, opts)
