8381c58ec67b5e777d7bbeb3c94db7354b32583e
Isaac Turner <spacey_sooty@outlook.com>
2024-01-13 19:37:14 +0800

    feat! some tab stuff + custom file creation :fire:
    Thanks to @Froze-N-Milk for inspiration and help with tab stuff

--- nvim/lua/spacey-sooty/remap.lua
+++ nvim/lua/spacey-sooty/remap.lua
@@ -1,5 +1,28 @@
-local keymap = vim.keymap
+local set = vim.keymap.set
+
 vim.g.mapleader = " "
 
-keymap.set('n', ';', ':')
-keymap.set('n', '<leader>pv', vim.cmd.Ex)
+set('n', ';', ':')
+set('n', '<leader><leader>m', vim.cmd.make)
+
+set('n', '<leader>pv', vim.cmd.Ex)
+set('n', '<leader>pt', vim.cmd.Tex)
+set('n', '<leader>n', vim.cmd.tabNext)
+
+set('n', '<leader>ts', "1gt")
+set('n', '<leader>td', "2gt")
+set('n', '<leader>tj', "3gt")
+set('n', '<leader>tk', "4gt")
+
+set('n', '<leader><leader>n', function()
+    local name = vim.fn.input('Name > ')
+    local path = vim.fn.getcwd() .. "/" .. name
+    vim.cmd("!touch " .. path)
+end)
+
+set('n', '<leader>hn', function()
+    local name = vim.fn.input('Name > ')
+    local path = vim.fn.getcwd() .. "/" .. name
+    vim.cmd("!touch " .. path)
+    require('harpoon.mark').add_file(name)
+end)
